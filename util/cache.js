ff91f6fb96bb8ab6eff584e547cd42dfdcf1dcac
Abdallah <abdallah31009448@f-eng.tanta.edu.eg>
2023-10-17 15:41:34 +0300

    move cache to .env

--- util/cache.js
+++ util/cache.js
@@ -1,7 +1,7 @@
 import { createClient } from 'redis'
 
 
-const cache = createClient({ url: 'redis://127.0.0.1:6379' });
+const cache = createClient({ url: process.env.CACHE_URL});
 
 cache.on('error', (err) => {
     console.log('Redis error: ', err);
